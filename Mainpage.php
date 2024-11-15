55ac05f7d9696c305b0ec8ed2e0ec15c01b99f33
ranjanR0412 <ranjanrohit1607@gmail.com>
2024-11-15 12:11:13 +0530

    Add files via upload

---
+++ Mainpage.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.0/css/bootstrap.min.css" integrity="sha384-SI27wrMjH3ZZ89r4o+fGIJtnzkAnFs3E4qz9DIYioCQ5l9Rd/7UAa8DHcaL8jkWt" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/rateYo/2.3.2/jquery.rateyo.min.css">
+    <video autoplay loop muted plays-inline class="bg-clip">
+    <source src="heart.mp4" type="video/mp4">
+</video>
+</head>
+
+ <body class="bg">
+
+
+<div>
+    <h1 class="title"> <span>F</span>inally </h1>
+    <h2 class="title2"> <span>Y</span>ou're </h2>
+    <h3 class="title3"> <span>O</span>ficially <span></span> <span>O</span>urs</h3>
+    <a class="button" href="Secondpage.php"><span>C</span>lick <span>h</span>ere <span>f</span>or <span>a</span> <span>r</span>ide</a>
+</div>
+    
+</body> 
+</html>
+
+
